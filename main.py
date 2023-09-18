19a7f2dfd5657a35f803bea9467b016a9fc5d0fd
misolove <hzlee73@naver.com>
2023-09-18 11:27:22 +0900

    feat: Functionalize and Edit mesage

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 
-for i in range(5):
-	print(f'hello for {i+1}th times')
+def print_hello(num: int):
+	
+	for i in range(num):
+		print(f'{i}번쨰 안녕')
+
+if __name__=='__main__':
+	print_hello(10)
 
 
